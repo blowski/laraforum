505011750a68ba8962fcac1f80780d0021cde6d9
Dan Blows <d@nblo.ws>
2019-10-06 21:31:48 +0100

    Fixed merge

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -40,10 +40,4 @@ class RepliesController extends Controller
         $this->authorize('update', $reply);
         $reply->update(['body' => request()->get('body')]);
     }
-
-    public function update(Reply $reply)
-    {
-        $this->authorize('update', $reply);
-        $reply->update(['body' => request()->get('body')]);
-    }
 }
